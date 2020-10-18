e9e81053288c6feb4f72e6b920d1adcfab255ed7
Pinkmanf <2538424511@qq.com>
2020-10-18 22:49:52 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int n = scr.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
